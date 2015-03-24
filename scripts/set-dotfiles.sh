1c2e4599df4195e31a59a2941f94129930903a4c
statox <adrien.fabre92@gmail.com>
2015-03-24 09:49:19 +0100

    renamed file

---
+++ scripts/set-dotfiles.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Author: Adrien Fabre (statox)
+#
+# This script is to use after cloning the repo
+# it save the original dotfiles in ./saved-dotfiles
+# and call the make-symlink.sh script to replace them
+
+
+# make a save of the current dotfiles
+sh ./save-dotfiles.sh
+
+# creating symlinks 
+sh ./make-symlinks.sh
+
+# source the .bashrc
+reloadbashrc
