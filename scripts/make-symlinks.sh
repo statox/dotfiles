56b758e71ca85c9fbb2ab0b00ce4569512e3a2f0
statox <adrien.fabre92@gmail.com>
2015-04-05 19:42:44 +0200

    Now ignore empty lines and commented lines in files_list are ignored

--- scripts/make-symlinks.sh
+++ scripts/make-symlinks.sh
@@ -7,14 +7,18 @@
 FILES=~/.dotfiles/scripts/files_list
 CUR_DIR=$(pwd)
 
+echo "creating the simlinks"
+
 # go to home directory
 cd ~
 
-# for each file in the list make a symlink
-for f in $(cat $FILES)
-do
-  ln -s .dotfiles/$f .$f
-done
+while read -r line; do
+    # ignore empty lines and comments in files_list
+    [[ -z $line  ]] && continue
+    [[ "$line" =~ ^#.*$  ]] && continue
+        echo "${line}"
+        ln -s .dotfiles/$line .$line
+done < "$FILES"
 
 # go back to original directory
 cd $CUR_DIR
--- scripts/save-dotfiles.sh
+++ scripts/save-dotfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 
+#
 # Author: Adrien Fabre (statox)
 #
 # This script makes a copy of dotfiles in home directory
@@ -11,29 +11,29 @@ DATE=$(date +%y%m%d_%H%M%S)
 
 SAVE_DIR=$SAVE_DIR/$DATE
 
+echo "saving current dotfiles"
+
 if [ ! -d $SAVE_DIR ]; then
-    echo "create dir $SAVE_DIR"
+    echo "Creating saving directory: $SAVE_DIR"
     mkdir -p $SAVE_DIR
 else
-    echo "$SAVE_DIR already exists"
+    echo "Saving directory already exists: $SAVE_DIR"
 fi
 
-echo "saving files"
 
-for f in $(cat $FILES)
-do
-  echo "   " $f
-  if [ -d ~/.$f ]; then
-    echo "directory"
-    cp -Lr ~/.$f $SAVE_DIR/$f
-    rm -rf ~/.$f
-  elif [ -f ~/.$f ]; then
-    echo "file"
-    cat ~/.$f > $SAVE_DIR/$f
-    rm -f ~/.$f
-  else
-    echo "not found"
-  fi
-  done
-
-echo "done"
+while read -r line; do
+# ignore empty lines and comments in files_list
+[[ -z $line  ]] && continue
+[[ "$line" =~ ^#.*$  ]] && continue
+    if [ -d ~/.$line ]; then
+        echo "directory ${line}"
+        cp -Lr ~/.$line $SAVE_DIR/$line
+        rm -rf ~/.$line
+    elif [ -f ~/.$line ]; then
+        echo "file ${line}"
+        cat ~/.$line > $SAVE_DIR/$line
+        rm -f ~/.$line
+    else
+        echo "not found ${line}"
+    fi
+done < "$FILES"
