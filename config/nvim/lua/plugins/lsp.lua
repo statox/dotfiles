02417f165dff2ba4a63276e3106a60604d4c4d50
Adrien Fabre <adrien.fabre@dashlane.com>
2024-08-13 18:29:07 +0200

    Clean up and comment LSP config

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -10,7 +10,6 @@ local function organize_imports()
 end
 
 -- on_attach function used after the language server attaches to the current buffer
----@diagnostic disable-next-line: unused-local
 local on_attach = function(client, bufnr)
     -- Enable completion triggered by <c-x><c-o>
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
@@ -19,8 +18,7 @@ local on_attach = function(client, bufnr)
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local bufopts = { noremap = true, silent = true, buffer = bufnr }
     vim.keymap.set('n', 'gd', '<cmd>Telescope lsp_definitions<CR>', bufopts)
-    vim.keymap.set('n', 'gD', ":vsplit <CR>: lua vim.lsp.buf.definition() <CR>: wincmd T<CR>",
-        { silent = true, buffer = bufnr })
+    vim.keymap.set('n', 'gD', ":vsplit <CR>: lua vim.lsp.buf.definition() <CR>: wincmd T<CR>", bufopts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
@@ -60,6 +58,15 @@ local lsp_flags = {
     debounce_text_changes = 150,
 }
 
+-- mason-lspconfig's `ensure_installed` option doesn't support packages
+-- other than LSP servers https://github.com/williamboman/mason-lspconfig.nvim/issues/83
+-- So the folowing formatters needs to be installed through the :Mason
+-- command interface
+
+-- prettier
+-- prettierd
+-- stylua
+
 local serversToInstall = {
     'ansiblels',
     'bashls',
@@ -68,12 +75,10 @@ local serversToInstall = {
     'eslint',
     'html',
     'prosemd_lsp', -- markdown
-    -- 'prettier',
+    'sqlls',
     'svelte',
     'terraformls',
     'tsserver',
-    --'sumneko_lua',
-    'sqlls',
     'vimls'
 }
 
@@ -85,7 +90,6 @@ require("mason-lspconfig").setup({
 })
 
 -- Set up cpm_nvim_lsp as a completion source for lsp servers
--- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- Set up the different lsp server installed by mason with lspconfig
@@ -104,17 +108,6 @@ for _, server in ipairs(serversToInstall) do
             }
         }
     end
-
-    --if server == 'sumneko_lua' then
-    --    opts.settings = {
-    --        Lua = {
-    --            diagnostics = {
-    --                -- Avoid "undefined global" warnings for vim in neovim config
-    --                globals = { 'vim' }
-    --            }
-    --        }
-    --    }
-    --end
 
     require('lspconfig')[server].setup(opts)
 end
