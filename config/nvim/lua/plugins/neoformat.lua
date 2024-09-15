dafdcf3f2e60dc258b06baf35d345522261dbe46
Adrien Fabre <adrien.fabre@dashlane.com>
2024-09-15 18:22:34 +0200

    Neoformat - Update config for various filetypes

--- config/nvim/lua/plugins/neoformat.lua
+++ config/nvim/lua/plugins/neoformat.lua
@@ -6,10 +6,19 @@
 local neoformatAG = vim.api.nvim_create_augroup("Neoformat", { clear = true })
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     desc = "Automatically format buffer on save with Neoformat plugin",
-    pattern = { "*.js", "*.ts", "*.tsx", "*.lua" },
+    -- pattern = { "*.js", "*.ts", "*.tsx", "*.lua", "*.ino", "*.cpp", "*.h" },
+    pattern = { "*" },
     group = neoformatAG,
     command = "Neoformat",
 })
+
+vim.api.nvim_create_user_command("NeoformatToggleVerbose", function()
+    if vim.g.neoformat_verbose == 1 then
+        vim.g.neoformat_verbose = 0
+    else
+        vim.g.neoformat_verbose = 1
+    end
+end, {})
 
 -- Try to find the binary of the formater in a node_modules/.bin
 -- useful for eslint and prettier
@@ -18,3 +27,19 @@ vim.g.neoformat_try_node_exe = 1
 
 -- Run all enabled formatters (by default stops after the first formatter succeeds)
 vim.g.neoformat_run_all_formatters = 1
+
+vim.g.neoformat_enabled_typescript = { "eslint_d", "prettierd" }
+vim.g.neoformat_enabled_typescriptreact = { "eslint_d", "prettierd" }
+vim.g.neoformat_enabled_javascript = { "eslint_d", "prettierd" }
+
+vim.g.neoformat_enabled_cpp = { "clang-format" }
+vim.g.neoformat_enabled_arduino = { "clang-format" }
+
+vim.g.neoformat_enabled_lua = { "stylua" }
+
+vim.g.neoformat_json_jq = {
+    exe = "jq",
+    args = { "--indent 4" },
+    stdin = true,
+}
+vim.g.neoformat_enabled_json = { "jq" }
