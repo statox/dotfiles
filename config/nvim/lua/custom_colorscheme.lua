22d9dcc86a8150fdf65df57e92954d3e719e8da1
statox <me@statox.fr>
2025-04-02 17:09:26 +0200

    Configure light mode

--- config/nvim/lua/custom_colorscheme.lua
+++ config/nvim/lua/custom_colorscheme.lua
@@ -2,7 +2,7 @@ if vim.fn.has("termguicolors") then
     vim.opt.termguicolors = true
 end
 
-vim.g.colorsDefault = "nightfox"
-vim.g.colorsDiff = "nordfox"
+vim.g.colorsDefault = "dayfox"
+vim.g.colorsDiff = "dayfox"
 
 vim.cmd("colorscheme " .. vim.g.colorsDefault)
