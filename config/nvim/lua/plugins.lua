7d9e38f59c2d291d83fa8e7e67a03490f6b94ac3
statox <me@statox.fr>
2025-10-02 18:07:29 +0200

    nvim - Add render-mardown plugin

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -97,6 +97,16 @@ require("packer").startup(function(use)
             vim.fn["mkdp#util#install"]()
         end,
     })
+    use({
+        'MeanderingProgrammer/render-markdown.nvim',
+        after = { 'nvim-treesitter' },
+        -- requires = { 'nvim-mini/mini.nvim', opt = true }, -- if you use the mini.nvim suite
+        -- requires = { 'nvim-mini/mini.icons', opt = true }, -- if you use standalone mini plugins
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }, -- if you prefer nvim-web-devicons
+        config = function()
+            require('render-markdown').setup({})
+        end,
+    })
 
     -- stevearc/aerial.nvim: Opens a split with the symbols of the current file
     -- to help navigating the file quickly
