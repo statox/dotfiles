af03ec8abcebbd5768749f53d831d8fe2092da26
statox <me@statox.fr>
2025-10-05 21:24:21 +0200

    wezterm - split ui settings in file

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
 local colorscheme = require 'colorscheme'
+local ui = require 'ui'
 
 local config = wezterm.config_builder()
 
@@ -68,39 +69,7 @@ wezterm.on("window-config-reloaded", function(window, pane)
   window:toast_notification("wezterm", "Config reloaded!", nil, 4000)
 end)
 
--- tab bar
-config.window_frame = {
-    -- The font used in the tab bar.
-    -- Roboto Bold is the default; this font is bundled
-    -- with wezterm.
-    -- Whatever font is selected here, it will have the
-    -- main font setting appended to it to pick up any
-    -- fallback fonts you may have used there.
-    font = wezterm.font { family = 'Roboto', weight = 'Bold' },
-
-    -- The size of the font in the tab bar.
-    -- Default to 10.0 on Windows but 12.0 on other systems
-    font_size = 12.0,
-
-    -- The overall background color of the tab bar when
-    -- the window is focused
-    active_titlebar_bg = '#333333',
-
-    -- The overall background color of the tab bar when
-    -- the window is not focused
-    inactive_titlebar_bg = '#333333',
-}
-
-config.tab_bar_at_bottom = true
-config.use_fancy_tab_bar = false
-
--- Window padding
-config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
-}
+ui.setup_ui(config)
 
 -- Finally, return the configuration to wezterm:
 return config
