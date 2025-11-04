2a8c7573cd3e680e7b4bd37a5a375cf48eb727e5
statox <me@statox.fr>
2025-11-04 20:46:22 +0100

    Wezterm - Tweak the name of the tabs in tab bar

--- config/wezterm/ui.lua
+++ config/wezterm/ui.lua
@@ -1,19 +1,18 @@
 local wezterm = require 'wezterm'
 local colorscheme = require 'colorscheme'
 
--- This function returns the suggested title for a tab.
--- It prefers the title that was set via `tab:set_title()`
--- or `wezterm cli set-tab-title`, but falls back to the
--- title of the active pane in that tab.
--- TODO Maybe rework to show the tab cwd
+
+function basename(s)
+    return string.gsub(s, '(.*[/\\])(.*)', '%2')
+end
+
 function tab_title(tab_info)
-    local title = tab_info.tab_title
-    -- if the tab title is explicitly set, take that
-    if title and #title > 0 then
-        return title
-    end
-    -- Otherwise, use the title from the active pane in that tab
-    return tab_info.active_pane.title
+    local pane = tab_info.active_pane
+    local cwd = pane.current_working_dir
+    local cwd_string = tostring(cwd)
+    local title = basename(cwd_string)
+
+    return title
 end
 
 wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
