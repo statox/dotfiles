16e142ffe332390ffe4da4b4f4493f3582450c58
statox <me@statox.fr>
2026-01-15 22:55:02 +0100

    nvim - Telescope - Stop using old branch, follow main

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -137,7 +137,6 @@ require("packer").startup(function(use)
     -- nvim-telescope/telescope.nvim: Fuzzy finder
     use({
         "nvim-telescope/telescope.nvim",
-        branch = "0.1.x",
         requires = {
             "nvim-lua/plenary.nvim",
             "nvim-telescope/telescope-fzf-native.nvim",
