a809209c2bf4df8b237a4792226f80e02aa88d25
statox <me@statox.fr>
2026-01-16 02:31:26 +0100

    nvim - markdown-preview - Fix broken configuration after migrating to lazy.nvim

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -110,9 +110,21 @@ local plugins = {
 
     -- Markdown preview with :MarkdownPreview
     {
+        -- Install markdown preview, use npx if available.
+        -- https://github.com/iamcco/markdown-preview.nvim/issues/690#issuecomment-2254280534
         "iamcco/markdown-preview.nvim",
-        build = function()
-            vim.fn["mkdp#util#install"]()
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function(plugin)
+            if vim.fn.executable "npx" then
+                vim.cmd("!cd " .. plugin.dir .. " && cd app && npx --yes yarn install")
+            else
+                vim.cmd [[Lazy load markdown-preview.nvim]]
+                vim.fn["mkdp#util#install"]()
+            end
+        end,
+        init = function()
+            if vim.fn.executable "npx" then vim.g.mkdp_filetypes = { "markdown" } end
         end,
     },
 
