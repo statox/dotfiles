debb8183256ad25d084e86cc65890eb01cb7834f
statox <me@statox.fr>
2026-06-07 00:25:30 +0200

    nvim - Extract markdown-preview config to its own file

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -142,9 +142,7 @@ local plugins = {
             end
         end,
         init = function()
-            if vim.fn.executable "npx" then vim.g.mkdp_filetypes = { "markdown" } end
-            vim.g.mkdp_markdown_css = vim.fn.stdpath("config") .. "/lua/markdown/style.css"
-            vim.g.mkdp_highlight_css = vim.fn.stdpath("config") .. "/lua/markdown/highlight.css"
+            require("plugins.markdown-preview")
         end,
     },
 
---
+++ config/nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,6 @@
+if vim.fn.executable("npx") then
+    vim.g.mkdp_filetypes = { "markdown" }
+end
+vim.g.mkdp_markdown_css = vim.fn.stdpath("config") .. "/lua/markdown/style.css"
+vim.g.mkdp_highlight_css = vim.fn.stdpath("config") .. "/lua/markdown/highlight.css"
+vim.g.mkdp_auto_close = true
